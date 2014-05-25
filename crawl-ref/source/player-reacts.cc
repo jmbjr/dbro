8021f9619eafead7ae08b66b1f5d82cedadfd2c6
John Boyle <boylejm@gmail.com>
2014-05-25 02:50:16 -0400

    use you.sicken to add SICK dur post Intox

--- crawl-ref/source/player-reacts.cc
+++ crawl-ref/source/player-reacts.cc
@@ -779,8 +779,7 @@ static void _decrement_durations()
 
         int dur = 12 + roll_dice(2, 12);
         you.increase_duration(DUR_EXHAUSTED, dur * 2);
-        you.disease = dur * 50;
-        you.increase_duration(DUR_SICKENING, dur * 50);
+        you.sicken(dur * 20);
         //TODO add hints
         // Don't trigger too many hints mode messages.
         // const bool hints_slow = Hints.hints_events[HINT_YOU_ENCHANTED];
